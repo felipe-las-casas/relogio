ee433414223929524b43a4d27901a6588b4dd56f
Felipe Las Casas <48955923+felipe-las-casas@users.noreply.github.com>
2022-04-19 15:30:53 -0300

    Add files via upload

---
+++ relogio/js/app.js
@@ -0,0 +1,18 @@
+const startTime = () => {
+    const today = new Date()
+    let h = today.getHours()
+    let m = today.getMinutes()
+    let s = today.getSeconds()
+    let ms = today.getMilliseconds()
+    m = checkTime(m)
+    s = checkTime(s)
+    document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s 
+    setTimeout(startTime, 1000);
+}
+  
+const checkTime = (i) => {
+    if (i < 10) {
+        i = "0" + i
+    }
+    return i;
+}
